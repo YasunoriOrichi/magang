b157a2d4056e2bcc6910c20a683e65c1b59feace
YasunoriOrichi <nurserinevil@gmail.com>
2025-06-04 11:24:50 +0700

    UPDATE #5.2
    Back up data, karena sebelumnya error

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
+<?php
+define('ENV_PATH', __DIR__ . '/config/env.php');
+require_once ENV_PATH;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="0; url=connect.php">
+    <!-- <meta http-equiv="refresh" content="0; url=connect.php"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
     <title>Document</title>
 </head>
 <body>
